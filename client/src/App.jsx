87e1606238db3136a276c8b88ded6336e43fdec8
GYosifov88 <gyosifov88@gmail.com>
2023-12-05 17:56:44 +0200

    add route guards

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,12 +14,13 @@ import GalleryAddItem from './components/GalleryAddItem/GalleryAddItem';
 import GalleryItemDetails from './components/GalleryItemDetails/GalleryItemDetails';
 import GalleryItemEdit from './components/GalleryItemEdit/GalleryItemEdit';
 import MyAccount from "./components/MyAccount/MyAccount";
-import MyAccountEdit from "./components/MyAccountEdit/MyAccountEdit";
+import MyAccountGallery from "./components/MyAccountGallery/MyAccountGallery";
 import Register from './components/Register/Register';
 import Login from './components/Login/Login';
 import Logout from './components/Logout/Logout';
 import NotFound from './components/NotFound/NotFound';
 import EventDetails from './components/EventDetails/EventDetails';
+import AuthGuard from './components/guards/AuthGuard';
 
 
 function App() {
@@ -38,14 +39,16 @@ function App() {
         <Route path={Path.EventDetails} element={<EventDetails />} />
         <Route path={Path.Story} element={<Story />} />
         <Route path={Path.Gallery} element={<Gallery />} />
-        <Route path={Path.GalleryAdd} element={<GalleryAddItem />} />
+        <Route element={<AuthGuard />}>
+            <Route path={Path.GalleryAdd} element={<GalleryAddItem />} />
+            <Route path={Path.GalleryItemEdit} element={<GalleryItemEdit />} />
+            <Route path={Path.Logout} element={<Logout />} />
+            <Route path={Path.MyAccountGallery} element={<MyAccountGallery />} />
+        </Route>
         <Route path={Path.GalleryItemId} element={<GalleryItemDetails />} />
-        <Route path={Path.GalleryItemEdit} element={<GalleryItemEdit />} />
         <Route path={Path.MyAccount} element={<MyAccount />} />
-        <Route path={Path.MyAccountEdit} element={<MyAccountEdit />} />
         <Route path={Path.Register} element={<Register />} />
         <Route path={Path.Login} element={<Login />} />
-        <Route path={Path.Logout} element={<Logout />} />
         <Route path={Path.Page404} element={<NotFound />} />
       </Routes>
 
